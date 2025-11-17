d0ec44f17a4b053707688d49a925bd58d39f6545
Daniel Arredondo Salcedo <daniels@cimat.mx>
2025-11-17 16:26:35 -0600

    Create Hola.java

---
+++ Hola.java
@@ -0,0 +1,9 @@
+public class Hola {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo");
+        System.out.println("Atte. Daniel");
+    }
+    
+}
